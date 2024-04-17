e66ce3dc4266ee476176f74e8b3cf35709077edd
sc11046 <121782720+sc11046@users.noreply.github.com>
2024-04-17 21:29:36 +0900

    20240417

--- coding test/체스/체스.c
+++ coding test/체스/체스.c
@@ -3,7 +3,7 @@
 int main(){
     int selNum[5];
 
-    printf("숫자 입력 \n");
+    printf("숫자 입력 : \n");
     scanf( "%d %d %d %d %d %d", &selNum[0],&selNum[1],&selNum[2],&selNum[3],&selNum[4],&selNum[5]);
     printf(" 입력한 숫자는? :%d %d %d %d %d %d\n", 1-selNum[0],1-selNum[1],2-selNum[2],2-selNum[3],2-selNum[4],8-selNum[5]);
     
